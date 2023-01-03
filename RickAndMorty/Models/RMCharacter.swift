d498a69f3bb997d01ddcb2ed207472dac4f587bd
robin-shahriar <24678311+shahriar-robin@users.noreply.github.com>
2023-01-03 16:30:23 +0600

    Initial commit

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Shahriar Robin on 3/1/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Shahriar Robin on 3/1/23.
+//
+
+import Foundation
