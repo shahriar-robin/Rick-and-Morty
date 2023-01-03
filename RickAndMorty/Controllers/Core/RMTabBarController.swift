8a9d411e096ac3cd6bbfb5f8ea23515b09fd6771
robin-shahriar <24678311+shahriar-robin@users.noreply.github.com>
2023-01-03 16:32:34 +0600

    core file add

--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RMTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupTabs()
     }
-
-
+    
+    private func setupTabs() {
+        let characterVC = RMCharacterViewController()
+        let locationVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        let nav1 = UINavigationController(rootViewController: characterVC)
+        let nav2 = UINavigationController(rootViewController: locationVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
 }
 
